4d272962c09e71cb4167cf12020960d5fc4e65fe
Tanjil <tanjilkh@gmail.com>
2024-06-12 18:39:10 +0100

    input para inserir e pesquisar um user

--- db/script.py
+++ db/script.py
@@ -51,5 +51,19 @@ def pesquisarUserUsername(Username):
         print(res)
 
 setupDB("managment_crud_users")
-inserirUtilizador("tskxz", "tanjil", "khan", "tanjil@gmail.com", "Rua de Sobreira 505", 123412341, "utilizador comum")
-pesquisarPrimeiroDocumento()
+input_insertornah = int(input("Queres inserir um user? [0/1]: "))
+if input_insertornah == 1:
+    username = input("Username: ")
+    primeiro_nome = input("Primeiro nome: ")
+    ultimo_nome = input("Ultimo nome: ")
+    email = input("Email: ")
+    morada = input("Morada: ")
+    telefone = input("Telefone: ")
+    descricao = input("Descricao: ")
+inserirUtilizador(username, primeiro_nome, ultimo_nome, email, morada, telefone, descricao)
+
+input_pesquisarornah = int(input("Queres pesquisar um user? [0/1]: "))
+if input_pesquisarornah == 1:
+    nomedogajo = str(input("Diz o nome do gajo: "))
+    pesquisarUserUsername(nomedogajo)
+    
