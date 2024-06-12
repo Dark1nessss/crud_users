008adc4351e3a34e2eb4e359c2dba6549ee84e28
Tanjil <tanjilkh@gmail.com>
2024-06-12 19:16:01 +0100

    rota api - mostrar os users

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-# Temos de adicionar aqui a ligacao ao mongo...
+app.config["MONGO_URI"] = "mongodb://localhost:27017/managment_crud_users"
+mongo = PyMongo(app)
 
 
 # memoria local
@@ -35,6 +37,15 @@ def delete_user(user_id):
     global users
     users = [user for user in users if user["id"] != user_id]
     return redirect(url_for("index"))
+
+# Rotas API users
+@app.route("/api/users", methods=["GET"])
+def api_users():
+    utilizadores_mongodb = mongo.db.utilizadores.find()
+    utilizadores = list(utilizadores_mongodb)
+    for i in utilizadores:
+        print(i)
+    return render_template("utilizadores.html", utilizadores=utilizadores)
 
 if __name__ == "__main__":
     app.run(debug=True)
